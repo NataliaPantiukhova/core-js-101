d947e7639996fe70015495eebd7de5aa1b2d25cc
N.Pantiukhova <NataliaPantiukhova@users.noreply.github.com>
2020-12-09 14:58:53 +0200

    feat: resolve conditions and loops task

--- src/08-conditions-n-loops-tasks.js
+++ src/08-conditions-n-loops-tasks.js
@@ -351,8 +351,19 @@ function getDigitalRoot(num) {
  *   '{)' = false
  *   '{[(<{[]}>)]}' = true
  */
-function isBracketsBalanced(/* str */) {
-  throw new Error('Not implemented');
+function isBracketsBalanced(str) {
+  const open = '[{(<'.split(''); const closed = ']})>'.split('');
+  return str.split('').reduce((a, b) => {
+    const stack = a;
+    if (!stack.length) { stack.push(b); return stack; }
+    if (open.includes(b)) { stack.push(b); } else {
+      const i = closed.indexOf(b);
+      if (stack.slice(-1)[0] === open[i]) { stack.pop(); } else {
+        stack.push(b);
+      }
+    }
+    return stack;
+  }, []).length === 0;
 }
 
 
